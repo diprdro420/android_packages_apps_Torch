9a486f237e4ca8878a5e66af36fa34c95096b096
Jubakuba <Jubakuba@gmail.com>
2014-01-15 03:10:55 -0700

    Torch: Glowpad Torch (2/3)
    Allow on / off functions to be
    	sent to torch.
    
    Change-Id: I5d090b00b6ea54ebed5057ba353b3e9c15b71d51
    
    Conflicts:
    	AndroidManifest.xml
    	src/net/cactii/flash2/TorchSwitch.java

---
+++ src/net/cactii/flash2/TorchSwitch.java
@@ -0,0 +1,82 @@
+/*
+ * Copyright (C) 2014 The CyanogenMod Project
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * version 3 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA  02110-1301, USA.
+ */
+
+package net.cactii.flash2;
+
+import android.app.Activity;
+import android.app.ActivityManager;
+import android.app.ActivityManager.RunningServiceInfo;
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+import java.util.List;
+
+public class TorchSwitch extends BroadcastReceiver {
+
+    public static final String TOGGLE_FLASHLIGHT = "net.cactii.flash2.TOGGLE_FLASHLIGHT";
+    public static final String FLASHLIGHT_OFF = "net.cactii.flash2.FLASHLIGHT_OFF";
+    public static final String FLASHLIGHT_ON = "net.cactii.flash2.FLASHLIGHT_ON";
+    public static final String TORCH_STATE_CHANGED = "net.cactii.flash2.TORCH_STATE_CHANGED";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        String action = intent.getAction();
+        // bright setting can come from intent or from prefs depending on
+        // on what send the broadcast
+        //
+        // Unload intent extras if they exist:
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+        boolean bright = intent.getBooleanExtra("bright", prefs.getBoolean("bright", false));
+        boolean stop = intent.getBooleanExtra("stop", false);
+
+        Intent i = new Intent(context, TorchService.class);
+        if (action.equals(TOGGLE_FLASHLIGHT)) {
+            if (stop || torchServiceRunning(context)) {
+                if (stop) {
+                    FlashDevice.instance(context).setFlashMode(FlashDevice.OFF);
+                }
+                context.stopService(i);
+            } else {
+                i.putExtra("bright", bright);
+                context.startService(i);
+            }
+        } else if (action.equals(FLASHLIGHT_ON)) {
+            i.putExtra("bright", bright);
+            context.startService(i);
+        } else if (action.equals(FLASHLIGHT_OFF)) {
+            context.stopService(i);
+        }
+    }
+
+    private boolean torchServiceRunning(Context context) {
+        ActivityManager am = (ActivityManager) context.getSystemService(Activity.ACTIVITY_SERVICE);
+        List<ActivityManager.RunningServiceInfo> svcList = am.getRunningServices(100);
+
+        for (RunningServiceInfo serviceInfo : svcList) {
+            ComponentName serviceName = serviceInfo.service;
+            if (serviceName.getClassName().endsWith(".TorchService")
+                    || serviceName.getClassName().endsWith(".RootTorchService"))
+                return true;
+        }
+        return false;
+    }
+}
